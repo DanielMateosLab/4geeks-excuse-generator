93338dfc8517de346c38609729507d9f2bac5662
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2021-12-11 20:06:51 +0000

    Select the random excuse components, create a string with them and update the excuse paragraph

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["The cat", "My partner", "His beard", "My stove"];
+let action = ["ate", "ruined", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function selectRandomItemFromArray(arr) {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const excuseComponents = [who, action, what, when];
+
+  const randomExcuse = excuseComponents
+    .map(component => selectRandomItemFromArray(component))
+    .join(" ");
+
+  document.getElementById("excuse").innerText = randomExcuse;
 };
